f49dcc37b07802fed147825aac74b2a630dc24c3
DS4506 <davidsmith4506@gmail.com>
2025-11-03 20:27:06 -0600

    Initial Commit

---
+++ WearFitSuite/WearFitSuiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearFitSuiteApp.swift
+//  WearFitSuite
+//
+//  Created by Willie Earl on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WearFitSuiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
