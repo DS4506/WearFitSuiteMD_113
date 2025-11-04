80fd0b710dd10a942b810761b0ea3a4393b2c997
DS4506 <davidsmith4506@gmail.com>
2025-11-03 20:33:10 -0600

    1st

---
+++ WearFitWatch Watch App/WearFitWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearFitWatchApp.swift
+//  WearFitWatch Watch App
+//
+//  Created by Willie Earl on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WearFitWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
